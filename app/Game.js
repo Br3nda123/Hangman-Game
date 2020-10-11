5369d9dafb542ca5d9f912638fe4f594d0ac7441
Brandon Kunicki <62794932+Br3nda123@users.noreply.github.com>
2020-10-11 14:25:14 +0200

    Add files via upload

---
+++ app/Game.js
@@ -0,0 +1,102 @@
+import {
+   Quote
+} from "./Quote.js";
+
+class Game {
+   currentStep = 0;
+   lastStep = 7;
+
+   quotes = [{
+         text: "pan tadeusz",
+         category: "Utwór literacki",
+      },
+      {
+         text: "janko muzykant",
+         category: "Utwór literacki",
+      },
+      {
+
+         text: "akademia pana kleksa",
+         category: "Film",
+      },
+      {
+         text: "kariera nikodema dyzmy",
+         category: "Film",
+      }
+   ];
+
+   constructor({
+      letterWrapper,
+      categoryWrapper,
+      wordWrapper,
+      outputWrapper
+   }) {
+      this.letterWrapper = letterWrapper;
+      this.categoryWrapper = categoryWrapper;
+      this.wordWrapper = wordWrapper;
+      this.outputWrapper = outputWrapper;
+
+      const {
+         text,
+         category
+      } = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+      this.categoryWrapper.innerHTML = category;
+      this.quote = new Quote(text);
+   };
+
+   guess(letter, e) {
+      e.target.disabled = true;
+      if (this.quote.guess(letter)) {
+         this.drawQuote();
+      } else {
+         this.currentStep++;
+         document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
+         if (this.currentStep == this.lastStep) {
+            this.loosing();
+         };
+      };
+   };
+
+   drawLetters() {
+      for (let i = 0; i < 26; i++) {
+         const label = (i + 10).toString(36);
+         const button = document.createElement('button');
+         button.innerHTML = label;
+         button.addEventListener('click', (e) => this.guess(label, e));
+         this.letterWrapper.appendChild(button);
+      };
+   };
+
+   drawQuote() {
+      const content = this.quote.getContent();
+      this.wordWrapper.innerHTML = content;
+      if (!content.includes('_')) {
+         this.winning();
+      };
+   };
+
+   start() {
+      document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
+      this.drawLetters();
+      this.drawQuote();
+   };
+
+   winning() {
+      this.wordWrapper.innerHTML = 'GRATULAJCE! WYGRYWASZ! KONIEC GRY';
+      this.letterWrapper.innerHTML = '';
+   };
+
+   loosing() {
+      this.wordWrapper.innerHTML = 'NIESTETY PRZEGRYWASZ! TO KONIEC GRY';
+      this.letterWrapper.innerHTML = '';
+   };
+
+};
+const game = new Game({
+   letterWrapper: document.getElementById('letters'),
+   categoryWrapper: document.getElementById('category'),
+   wordWrapper: document.getElementById('word'),
+   outputWrapper: document.getElementById('output')
+});
+
+game.start();
